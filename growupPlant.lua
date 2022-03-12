5b0dd695a1cd5e1098e1dc1de56d19c8efadd727
Frederic Lossignol <frederic.lossignol@gmail.com>
2022-03-12 01:29:27 +0100

    Update growupPlant.lua

--- growupPlant.lua
+++ growupPlant.lua
@@ -1,3 +1,20 @@
+-- GrowUp Plant
+-- @Author: @NumericFactory (Frederic LOSSIGNOL)
+-- Configuration of the parcel
+local grass = script.Parent
+local posX = -1067;
+local posY = 13.25;
+local posZ = -649.5;
+local sizeX = 34
+local sizeZ = 40
+local pos = Vector3.new(posX, posY, posZ);
+local towerBaseSize = 30
+
+-- Decide on two colors that are different in hue
+local hue = 1/2
+local color0 = Color3.fromHSV(hue, 1, 1)
+local color1 = Color3.fromHSV((hue + .35) % 1, 1, 1)
+
 local grassLeftTopX = posX - sizeX/2
 local grassLeftTopZ = posZ - sizeZ/2
 
@@ -8,16 +25,17 @@ model.Name = "Plant"
 -- growUpThisPlant
 local function growUpAllPlants()
 	local children = script.Parent:FindFirstChild(model.Name):GetChildren()
-	for i = 1, #children do
-		local randomAddSize = math.random(50,180)
-		local child = children[i]
-		for count = 1, 30 do
+	for count = 15, 120 do
+		for i = 1, #children do		
+			local child = children[i]
+			--local addSize = child:GetAttribute("randomAddSize")
+			--local size = tonumber(addSize)*tonumber(count/10)
 			-- grow Up
 			local perc = count / 30
-			child.Size = Vector3.new(.25, randomAddSize/100*count/8, .25)
+			child.Size = Vector3.new(.2,  tonumber(child:GetAttribute("randomAddSize"))*tonumber(count/18), .2)
 			--child.Color = Color3.new(0, 1*perc, 0.498039*perc)
-			--wait(.005)
 		end
+		wait(.1)
 	end
 end
 
@@ -25,6 +43,7 @@ end
 local function createPlantsOnParcel() 
 	for countZ = 1, sizeZ, 2 do	
 		for count = 1, sizeX, 2 do
+			local randomAddSize = math.random(40,110)
 			local random = math.random(1,4)
 			local color = Color3.new(0.0941176, 0.992157, 0.498039)	
 			-- determinate color
@@ -39,11 +58,13 @@ local function createPlantsOnParcel()
 			local part = Instance.new("Part")
 			part.CanCollide = false
 			part.Parent = model
-			part.Size = Vector3.new(.2, .8, .2)
+			part.Size = Vector3.new(.2,randomAddSize/100*0.8, .2)
 			part.Position = Vector3.new(grassLeftTopX+count, 13.25, grassLeftTopZ+countZ)
 			part.Anchored = true
 			part.Transparency = 0
-			part.Color = color			
+			part.Color = color
+			part:SetAttribute('randomAddSize', randomAddSize/100)
+			
 		end
 	end
 end
@@ -58,6 +79,7 @@ local function growUpPlant(plantType)
 	part2.Anchored = true
 	part2.Transparency = 0
 	part2.Color = Color3.new(0, 0.686275, 0.329412)
+	part2:SetAttribute('randomAddSize', 100/100)
 	
 	-- create part of Plant
 	local part = Instance.new("Part")
@@ -67,6 +89,7 @@ local function growUpPlant(plantType)
 	part.Anchored = true
 	part.Transparency = 0
 	part.Color = Color3.new(0, 1, 0.498039)
+	part:SetAttribute('randomAddSize', 100/100)
 	-- Attach to parent part
 	model.Parent = grass
 	for count = 1, 150 do
