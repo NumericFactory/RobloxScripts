0f02e339c179faa8814565b7e24622f53f9e2380
Frederic Lossignol <frederic.lossignol@gmail.com>
2022-03-12 01:41:27 +0100

    Update growupPlant.lua

--- growupPlant.lua
+++ growupPlant.lua
@@ -21,23 +21,8 @@ local grassLeftTopZ = posZ - sizeZ/2
 -- Create a model to hold plant parts
 local model = Instance.new("Model")
 model.Name = "Plant"
-
--- growUpThisPlant
-local function growUpAllPlants()
-	local children = script.Parent:FindFirstChild(model.Name):GetChildren()
-	for count = 15, 120 do
-		for i = 1, #children do		
-			local child = children[i]
-			--local addSize = child:GetAttribute("randomAddSize")
-			--local size = tonumber(addSize)*tonumber(count/10)
-			-- grow Up
-			local perc = count / 30
-			child.Size = Vector3.new(.2,  tonumber(child:GetAttribute("randomAddSize"))*tonumber(count/18), .2)
-			--child.Color = Color3.new(0, 1*perc, 0.498039*perc)
-		end
-		wait(.1)
-	end
-end
+-- Attach to parent part
+model.Parent = grass
 
 -- create Plants on Parcel
 local function createPlantsOnParcel() 
@@ -63,52 +48,70 @@ local function createPlantsOnParcel()
 			part.Anchored = true
 			part.Transparency = 0
 			part.Color = color
-			part:SetAttribute('randomAddSize', randomAddSize/100)
-			
+			part:SetAttribute('randomAddSize', randomAddSize/100)	
+
 		end
 	end
 end
 
---growUpPlant
-local function growUpPlant(plantType)
-	-- create part of Plant
-	local part2 = Instance.new("Part")
-	part2.Parent = model
-	part2.Size = Vector3.new(.5, 1, .5)
-	part2.Position = Vector3.new(-1065, 13.25, -645.5)
-	part2.Anchored = true
-	part2.Transparency = 0
-	part2.Color = Color3.new(0, 0.686275, 0.329412)
-	part2:SetAttribute('randomAddSize', 100/100)
-	
-	-- create part of Plant
-	local part = Instance.new("Part")
-	part.Parent = model
-	part.Size = Vector3.new(.5, 1, .5)
-	part.Position = pos
-	part.Anchored = true
-	part.Transparency = 0
-	part.Color = Color3.new(0, 1, 0.498039)
-	part:SetAttribute('randomAddSize', 100/100)
-	-- Attach to parent part
-	model.Parent = grass
-	for count = 1, 150 do
-		-- grow Up
-		local perc = count / 150
-		part.Size = Vector3.new(.5, count/16, .5)
-		part.Color = Color3.new(0, 1*perc, 0.498039*perc)
-		part2.Size = Vector3.new(.5, count/18, .5)
-		part2.Color = Color3.new(0, 0.686275*perc,  0.329412*perc)
+-- growUpThisPlant
+local function growUpAllPlants()
+	wait(10)
+	local children = script.Parent:FindFirstChild(model.Name):GetChildren()
+	for count = 15, 120 do
+		for i = 1, #children do		
+			local child = children[i]
+			--local addSize = child:GetAttribute("randomAddSize")
+			--local size = tonumber(addSize)*tonumber(count/10)
+			-- grow Up
+			local perc = count / 30
+			child.Size = Vector3.new(.2,  tonumber(child:GetAttribute("randomAddSize"))*tonumber(count/18), .2)
+			--child.Color = Color3.new(0, 1*perc, 0.498039*perc)
+		end
 		wait(.1)
 	end
-	print('herbe')
 end
 
--- execute growup Plant
-createPlantsOnParcel() 
-growUpPlant('Herbe')
 
 print(grass.Position)
 print(grass.Size)
-
+-- execute create and grow up Plants
+createPlantsOnParcel() 
 growUpAllPlants()
+
+----------------
+-- TEST
+--growUpPlant
+--local function growUpPlant(plantType)
+--	-- create part of Plant
+--	local part2 = Instance.new("Part")
+--	part2.Parent = model
+--	part2.Size = Vector3.new(.5, 1, .5)
+--	part2.Position = Vector3.new(-1065, 13.25, -645.5)
+--	part2.Anchored = true
+--	part2.Transparency = 0
+--	part2.Color = Color3.new(0, 0.686275, 0.329412)
+--	part2:SetAttribute('randomAddSize', 100/100)
+
+--	-- create part of Plant
+--	local part = Instance.new("Part")
+--	part.Parent = model
+--	part.Size = Vector3.new(.5, 1, .5)
+--	part.Position = pos
+--	part.Anchored = true
+--	part.Transparency = 0
+--	part.Color = Color3.new(0, 1, 0.498039)
+--	part:SetAttribute('randomAddSize', 100/100)
+--	-- Attach to parent part
+--	model.Parent = grass
+--	for count = 1, 150 do
+--		-- grow Up
+--		local perc = count / 150
+--		part.Size = Vector3.new(.5, count/16, .5)
+--		part.Color = Color3.new(0, 1*perc, 0.498039*perc)
+--		part2.Size = Vector3.new(.5, count/18, .5)
+--		part2.Color = Color3.new(0, 0.686275*perc,  0.329412*perc)
+--		wait(.1)
+--	end
+--	print('herbe')
+--end
