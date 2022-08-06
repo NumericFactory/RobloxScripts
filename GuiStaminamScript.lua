b8f50bcb5c1821e2baeca955bb7b8b52da7ca310
Frederic Lossignol <frederic.lossignol@gmail.com>
2022-08-06 14:01:31 +0200

    Create GuiStaminamScript.lua

---
+++ GuiStaminamScript.lua
@@ -0,0 +1,72 @@
+-- @Author : Frederic LOSSIGNOL / Numeric Factory
+
+-- INSTRUCTIONS ----------------------
+
+-- 1 : create GUI in StarterGui folder
+--      > Create a frame and name it "Background"
+--      > Under "Background" Frame, Create a new frame, and name it StaminaDisplay
+-- 2 : Under "StaminaDisplay" Frame, Create a LocalScript, and copy/paste the following script 
+
+-- END INSTRUCTIONS -------------------
+
+local numberIndicatorTextLabel = script.Parent.Parent.NumberIndicatorLabel
+
+local player = game.Players.LocalPlayer
+local character = player.Character
+
+local UserInputService = game:GetService("UserInputService")
+
+local sprintSpeed = 26
+local walkSpeed = 16
+
+local stamina = 100
+local running = false
+
+local bindableEvent = game.Workspace:WaitForChild("isPlayerRunningEvent")
+
+stamina = math.clamp(stamina, 0, 100)
+
+UserInputService.InputBegan:Connect(function(input)
+	if input.KeyCode == Enum.KeyCode.LeftShift then
+		running = true
+		--character.Humanoid.WalkSpeed = sprintSpeed
+		while stamina > 0 and running do
+			stamina = stamina - 0.5
+			if stamina<=1  then
+				stamina=0
+			end
+			script.Parent:TweenSize(UDim2.new(stamina/100,0,1,0),"Out","Linear", 0)
+			if stamina == 0 then
+				numberIndicatorTextLabel.Text = "0"
+			else
+				numberIndicatorTextLabel.Text = (math.floor(stamina+0.5))
+			end
+			
+			wait()
+			if stamina == 0 then
+				character.Humanoid.WalkSpeed = walkSpeed
+				-------------------------
+				bindableEvent:Fire()
+				-------------------------
+			end
+			
+		end
+	end
+end)
+
+UserInputService.InputEnded:Connect(function(input)
+	if input.KeyCode == Enum.KeyCode.LeftShift then
+		running=false
+		character.Humanoid.WalkSpeed = walkSpeed
+		while stamina < 100 and not running do
+			stamina = stamina + 0.75
+			script.Parent:TweenSize(UDim2.new(stamina/100,0,1,0),"Out","Linear", 0)
+			numberIndicatorTextLabel.Text = (math.floor(stamina)) 
+			wait()
+			if stamina == 0  then
+				character.Humanoid.WalkSpeed = walkSpeed
+			end
+		end
+		
+	end
+end)
