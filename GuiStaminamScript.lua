138062307e38872728ec93bc370933d23606fbb4
Frederic Lossignol <frederic.lossignol@gmail.com>
2022-08-08 22:03:49 +0200

    Update GuiStaminamScript.lua

--- GuiStaminamScript.lua
+++ GuiStaminamScript.lua
@@ -8,9 +8,7 @@
 -- 2 : Under "StaminaDisplay" Frame, Create a LocalScript, and copy/paste the following script 
 
 -- END INSTRUCTIONS -------------------
-
-local numberIndicatorTextLabel = script.Parent.Parent.NumberIndicatorLabel
-
+local numberIndicatorTextLabel
 local player = game.Players.LocalPlayer
 local character = player.Character
 
@@ -22,12 +20,19 @@ local walkSpeed = 16
 local stamina = 100
 local running = false
 
-local bindableEvent = game.Workspace:WaitForChild("isPlayerRunningEvent")
+-- print bar with counter
+withCounter = false
+
+if withCounter then
+	numberIndicatorTextLabel = script.Parent.Parent.NumberIndicatorLabel
+end
+
+--local bindableEvent = game.Workspace:WaitForChild("isPlayerRunningEvent")
 
 stamina = math.clamp(stamina, 0, 100)
 
 UserInputService.InputBegan:Connect(function(input)
-	if input.KeyCode == Enum.KeyCode.LeftShift then
+	if input.KeyCode == Enum.KeyCode.LeftControl then
 		running = true
 		--character.Humanoid.WalkSpeed = sprintSpeed
 		while stamina > 0 and running do
@@ -36,37 +41,45 @@ UserInputService.InputBegan:Connect(function(input)
 				stamina=0
 			end
 			script.Parent:TweenSize(UDim2.new(stamina/100,0,1,0),"Out","Linear", 0)
-			if stamina == 0 then
-				numberIndicatorTextLabel.Text = "0"
-			else
-				numberIndicatorTextLabel.Text = (math.floor(stamina+0.5))
+			
+			if withCounter then
+				if stamina == 0 then
+					numberIndicatorTextLabel.Text = "0"
+				else
+					numberIndicatorTextLabel.Text = (math.floor(stamina+0.5))
+				end
 			end
 			
+
 			wait()
 			if stamina == 0 then
 				character.Humanoid.WalkSpeed = walkSpeed
 				-------------------------
-				bindableEvent:Fire()
+				--bindableEvent:Fire()
 				-------------------------
 			end
-			
+
 		end
 	end
 end)
 
 UserInputService.InputEnded:Connect(function(input)
-	if input.KeyCode == Enum.KeyCode.LeftShift then
+	if input.KeyCode == Enum.KeyCode.LeftControl then
 		running=false
 		character.Humanoid.WalkSpeed = walkSpeed
 		while stamina < 100 and not running do
 			stamina = stamina + 0.75
 			script.Parent:TweenSize(UDim2.new(stamina/100,0,1,0),"Out","Linear", 0)
-			numberIndicatorTextLabel.Text = (math.floor(stamina)) 
+			
+			if withCounter then
+				numberIndicatorTextLabel.Text = (math.floor(stamina)) 
+			end
+				
 			wait()
 			if stamina == 0  then
 				character.Humanoid.WalkSpeed = walkSpeed
 			end
 		end
-		
+
 	end
 end)
