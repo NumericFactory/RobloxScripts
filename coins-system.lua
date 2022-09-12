5fb569f1b4eefcd87bec71a87047d92c14fa3fc1
Frederic Lossignol <frederic.lossignol@gmail.com>
2022-09-12 13:55:02 +0200

    Update coins-system.lua

--- coins-system.lua
+++ coins-system.lua
@@ -1,3 +1,6 @@
+-- @Author : Frederic Lossigno - @NumericFactory
+-- Instructions : follow the 3 steps to create a coin system
+
 -----------------------------------------------------------------------
 -- STEP 1 : in ServerScriptService, create a script, 
 --          name it LeaderStatsScript, and copy/past the following code
@@ -14,8 +17,11 @@ end
 
 game.Players.PlayerAdded:Connect(onPlayerJoin)
 
+
+
 --------------------------------------------------------------------------------
--- STEP 2: create a part, name it "coin", 
+-- STEP 2: in workspace, create a folder, name it "coins"
+--         in this folder, create a part (or import mesh) and name it "MarioCoin", 
 --         create a new script in this part, and copy/paste thid following code
 --------------------------------------------------------------------------------
 -- Tween the position of a part when it's touched, then destroy it
@@ -56,7 +62,7 @@ local function collectCoin(otherPart)
 
 			local player = game.Players:FindFirstChild(otherPart.Parent.Name)
 			if player then
-				
+
 				sound:Play()
 				player.leaderstats.Coins.Value = player.leaderstats.Coins.Value+1
 
@@ -69,7 +75,7 @@ local function collectCoin(otherPart)
 				)
 				tweenObject:Play()
 				inTween = true
-				
+
 				-- On tween completion, destroy object
 				tweenObject.Completed:Connect(function()
 					coin:Destroy()
@@ -79,4 +85,45 @@ local function collectCoin(otherPart)
 	end	
 end
 
-coin.Touched:Connect(collect)
+coin.Touched:Connect(collectCoin)
+
+
+
+--------------------------------------------------------------------------------
+-- STEP 3: in workspace, create a part and name it SurfaceCoinsPart
+--         create a new script in this part, and copy/paste thid following code
+--------------------------------------------------------------------------------
+local surface = script.Parent
+surface.Transparency = 0.8
+
+local coins = workspace:WaitForChild("coins")
+local originaCoin = coins.MarioCoin;
+
+-- CUSTOM VARIABLES---------------------------------
+local density = 10 -- number of coins on the surface
+----------------------------------------------------
+
+local x = surface.Size.X/2
+local y = surface.Size.Y/2
+local z = surface.Size.Z/2
+
+local lastPositionCoin = Vector3.new(0,0,0)
+local isOnRight = false
+local isOnTop = false
+
+local function simpleSpawnCoin()
+	local copyCoin = originaCoin:Clone()
+	copyCoin.Position = surface.Position + Vector3.new(math.random(-x, x), 2.5, math.random(-z, z))
+	copyCoin.Name = "COIN"
+	copyCoin.Anchored=true
+	copyCoin.Parent = surface
+	return copyCoin
+end
+
+for i=1,density do
+	local newCoin = simpleSpawnCoin()
+end
+
+
+
+
