4cd21ca8ab6b01d07e42bb642be27abf9213fff0
Frederic Lossignol <frederic.lossignol@gmail.com>
2022-09-16 02:05:51 +0200

    Update open-door.lua

--- open-door.lua
+++ open-door.lua
@@ -1,7 +1,7 @@
 -- @Author : Fred Lossignol / NumericFactory
 -- Instructions : 
 -- 1 > create a door part, add a proximity prompt
--- 2 > add a script in doot part, and copy/past this code
+-- 2 > add a script in doot part, and copy/past this code 
 
 local Door = script.Parent
 local startCFrame = Door.CFrame
@@ -9,10 +9,11 @@ local dX2 = Door.Size.X / 2
 local isOpened = false
 
 -- CUSTOM VARIABLES
-local TIME_TO_OPEN = 5 -- number of seconds to open the door
-local TIME_TO_CLOSE = 1 -- number of seconds to close the door
+local TIME_TO_OPEN = 1 -- number of seconds to open the door
+local TIME_TO_CLOSE = 3 -- number of seconds to close the door
+local EASING = 1 -- acceleration: Choose 1 for "FastToLow" | Choose 2 for "Linear"
+local ANGLE = 100 -- angle de l'ouverture de la porte en degré (conseillé : 90° à 150° max)
 -- END CUSTOM VARIABLES
-
 
 
 local sounds = {
@@ -25,7 +26,7 @@ local sounds = {
 function CFrameAtPercentage(p)
 	return startCFrame 
 		* CFrame.new(-dX2, 0, 0) 
-		* CFrame.Angles(0, math.rad(90) * p, 0) 
+		* CFrame.Angles(0, math.rad(ANGLE) * p, 0) 
 		* CFrame.new(dX2, 0, 0)
 end
 
@@ -43,41 +44,53 @@ end
 -- Function openDoor() et closeDoor()
 local i = 0
 
-
-
+----------------------
+-- openDoor() Function
+----------------------
 function openDoor() 
 	sounds.doorOpen:Play()
 	local count = 0
 	while true do
-		i = LerpN(i, 90, 1/TIME_TO_OPEN/10)
-		--print(i)
-		Door.CFrame = CFrameAtPercentage(i/85)
+		-- déceleration animation
+		if EASING==1 then
+			i = LerpN(i, ANGLE, 1/TIME_TO_OPEN/10)
+		-- OU animation vitesse lieaire
+		else	
+			i=i+ANGLE*0.33/TIME_TO_OPEN/10
+		end
+		
+		Door.CFrame = CFrameAtPercentage(i/(ANGLE-5))
 		count = count+1
 		wait()
-		if i>=85 then
+		if i>=ANGLE-5 then
 			break
 		end
 	end
-	print(wait())
 	print(count)
 	Door.ProximityPrompt.ActionText = "Close"
-	isOpened = true
-
 end
 
+
+-----------------------
+-- closeDoor() Function
+-----------------------
 function closeDoor()
 	sounds.doorOpen:Play()
 	while true do
-		i = LerpN(i, -90, 1/TIME_TO_CLOSE/10)
-		--print(i)
-		Door.CFrame = CFrameAtPercentage(i/90)
+		-- déceleration animation
+		if EASING==1 then
+		i = LerpN(i,-5, 1/TIME_TO_CLOSE/10)
+		-- OU animation vitesse lieaire
+		else	
+			i=i-ANGLE*0.33/TIME_TO_OPEN/10
+		end
+		Door.CFrame = CFrameAtPercentage(i/(ANGLE-5))
 		wait()
 		if i<=1 then
 			break
 		end
 	end
 	Door.ProximityPrompt.ActionText = "Open"
-	isOpened = false
 end
 ------------------------------------
 -- Fin Function openDoor() et closeDoor()
@@ -85,26 +98,14 @@ end
 
 ---------------------------------------------------------
 -- CODE PRINCIPAL : QUAND LE PROXIMITY PROMPT EST ACTIVÉ PÄR LE PLAYE
-
 script.Parent.ProximityPrompt.Triggered:Connect(function()
-	--Door.CFrame=Door.CFrame*CFrame.Angles(0,45,0)
-	if isOpened == false then
-		
+	if isOpened == false then	
 		openDoor()
 		isOpened = true
 		--print(isOpened)
-		
-	else
-		
+	else	
 		closeDoor()
 		isOpened = false
-		--print(isOpened)
-		
+		--print(isOpened)	
 	end
-	
 end)
-
-
-
-
-
